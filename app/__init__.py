6f0e4bb26e05066c72849a7837bc056e61fde3f7
venombrend <venombrend@gmail.com>
2016-07-24 18:15:33 +0300

    completed adding post

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import os
-#from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-#bootstrap = Bootstrap(app)
+
 
 from app import views, database
 
--- app/views.py
+++ app/views.py
@@ -1,16 +1,12 @@
-from flask import render_template
-from flask.templating import render_template
-
 from app import app
 import database
 
 
-
-
 from app import app
 from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import request, session, g, redirect, url_for, abort, \
     render_template, flash
+from datetime import datetime
 
 
 
@@ -18,7 +14,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 @app.route('/index')
 def index():
     db = database.get_db()
-    sql_query = "select * from entries"
+    sql_query = "select * from entries order by entries.id desc"
     cur = db.execute(sql_query)
     posts = cur.fetchall()
     return render_template("show_posts.html", posts=posts)
@@ -46,13 +42,25 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/add_post')
+@app.route('/add_post', methods={'GET', 'POST'})
 def add_post():
+    if not session.get('logged_in'):
+        abort(401)
+    if request.method == 'POST':
+        db = database.get_db()
+        current_data = str(datetime.today())[:19]
+        db.execute("insert into entries (slug, title, short_text, full_text, timestamp) values (?, ?, ?, ?, ?)",
+                   ['post '+current_data, request.form['post-title'], request.form['post-short'],
+                    request.form['post-full'], current_data])
+        db.commit()
+        flash('New entry was successfully posted')
+        return redirect('index')
     return render_template('add_post.html')
 
 
 @app.route('/posts/<slug_post>')
 def read_post(slug_post):
+    """Show whole post """
     db = database.get_db()
     sql_query  = "select * from entries where entries.slug='{0}'".format(slug_post)
 
