1915a236364b764af8e50111108fe2e91795397b
venombrend <venombrend@gmail.com>
2016-07-25 22:42:41 +0300

    Fixed bug with display post text

--- app/views.py
+++ app/views.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-from flask import render_template
-from flask.templating import render_template
-
->>>>>>> 0b07e3c036c097cdd4aaecc6170f4c8f27c002f4
 from app import app
 import database
 
@@ -20,11 +14,7 @@ from datetime import datetime
 @app.route('/index')
 def index():
     db = database.get_db()
-<<<<<<< HEAD
     sql_query = "select * from entries order by entries.id desc"
-=======
-    sql_query = "select * from entries"
->>>>>>> 0b07e3c036c097cdd4aaecc6170f4c8f27c002f4
     cur = db.execute(sql_query)
     posts = cur.fetchall()
     return render_template("show_posts.html", posts=posts)
@@ -52,7 +42,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-<<<<<<< HEAD
 @app.route('/add_post', methods={'GET', 'POST'})
 def add_post():
     if not session.get('logged_in'):
@@ -66,23 +55,21 @@ def add_post():
         db.commit()
         flash('New entry was successfully posted')
         return redirect('index')
-=======
-@app.route('/add_post')
-def add_post():
->>>>>>> 0b07e3c036c097cdd4aaecc6170f4c8f27c002f4
     return render_template('add_post.html')
+
+
+@app.route('/edit_post/<slug_post>', methods=['GET', 'POST'])
+def edit_post(slug_post):
+    db = database.get_db()
+    return redirect('index')
+
 
 
 @app.route('/posts/<slug_post>')
 def read_post(slug_post):
-<<<<<<< HEAD
     """Show whole post """
-=======
->>>>>>> 0b07e3c036c097cdd4aaecc6170f4c8f27c002f4
     db = database.get_db()
     sql_query  = "select * from entries where entries.slug='{0}'".format(slug_post)
-
-
     cur = db.execute(sql_query)
     post = cur.fetchall()
     return render_template("read_post.html", posts=post)
